3c9f2c355aa862ad52fac0247175951edf5b0395
NerdWilly <95449663+nerdwilly@users.noreply.github.com>
2022-07-06 09:53:58 +0200

    Add files via upload

---
+++ gcphone/config.lua
@@ -0,0 +1,5 @@
+FixePhone = {
+  ['008-0001'] = { name = "Cabine Telephonique", coords = { x = 372.25, y = -965.75, z = -228.58 } },
+}
+
+ShowNumberNotification = false -- Show Number or Contact Name when you receive new SMS
